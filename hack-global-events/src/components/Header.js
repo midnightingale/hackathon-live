2e242942e1950fffaee2d3696ec9c2e7c6303604
sophie <sophiecliu@gmail.com>
2021-03-16 17:04:51 -0400

    Finish header styling

--- hack-global-events/src/components/Header.js
+++ hack-global-events/src/components/Header.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "../styles/header.css";
 import '../styles/global.css';
-import { Link } from 'react-router-dom'
+import hgi from "../assets/logo.svg";
 
 function Header({type}){
   return(
-    <div>
+    <div className="header-container">
+      <img src={hgi} className="logo-image"/>
       <h1>events</h1>   
     </div>
   )
