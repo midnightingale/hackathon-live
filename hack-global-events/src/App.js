7322ed3b1342ae19149e60b8a10009c0ff66c474
sophie <sophiecliu@gmail.com>
2021-03-16 19:53:35 -0400

    HTTP request and EventCard map working

--- hack-global-events/src/App.js
+++ hack-global-events/src/App.js
@@ -2,14 +2,27 @@ import './App.css';
 import React from "react"; 
 import Header from "./components/Header";
 import EventCard from "./components/EventCard";
+
+/*
+function App() {
   
+
+  useEffect(() => {
+    // GET request using fetch inside useEffect React hook
+    fetch('https://api.npms.io/v2/search?q=react')
+        .then(response => response.json())
+        .then(data => setTotalReactPackages(data.total));
+   }, []);
+}*/
+
 class App extends React.Component { 
   constructor(props) { 
     super(props); 
-    this.state = { 
+    this.state = {
+      events: [], 
       loggedIn: false,
       sidebarActive: false,
-      eventActive: null                 
+      eventActive: 0                 
     }; 
     this.event1 = {
       "id": 1,
@@ -34,15 +47,26 @@ class App extends React.Component {
     }
   } 
 
+  componentDidMount() {
+    fetch('https://api.hackthenorth.com/v3/graphql?query={ events { id name event_type permission start_time end_time description speakers { name profile_pic } public_url private_url related_events } }')
+        .then(response => response.json())
+        .then(dataIn => this.setState({events: dataIn.data.events})
+        )
+        .then(console.log(this.state));
+    
+  } 
+
   render() { 
     return ( 
-      <div> {/*leave this here*/}
-      <Header />   
-      <div className="events-container">
-      <EventCard event={this.event1}/> 
+      <div> 
+        <Header />   
+        <div className="events-container">
+        <EventCard event={this.event1}/> 
+          {this.state.events.map((event) => (
+            <EventCard event={event} />
+          ))}
+        </div>
       </div>
-         
-      </div> /*leave this here*/
     ); 
   } 
 } 
