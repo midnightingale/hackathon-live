e311bf4bca83504ea8d7173060490bffb7fb139f
sophie <sophiecliu@gmail.com>
2021-03-17 05:17:03 -0400

    Start login button files

--- src/components/LoginButton.js
+++ src/components/LoginButton.js
@@ -1,1 +1,24 @@
 //password is htn2021
+
+import React from "react";
+import "../styles/loginbutton.css";
+
+function LoginButton({type, location}){
+  return(
+    <div className={"login-button"}>
+      login
+    </div>
+  )
+}
+
+
+
+function openModal(){
+  document.getElementById("info-modal").style.display = "block";
+}
+
+function closeModal(){
+  document.getElementById("info-modal").style.display = "none";
+}
+
+export default LoginButton;
